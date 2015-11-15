af980517535f9ee4f62fddb70e1762fb97b98bb4
KevinCepria <cepria21@yahoo.com>
2015-11-15 15:30:28 +0800

    Weekday app using datepicker

---
+++ app/src/main/java/ph/edu/dlsu/datepickerfragment/MyInterface.java
@@ -0,0 +1,18 @@
+package ph.edu.dlsu.datepickerfragment;
+
+import android.widget.TextView;
+
+/**
+ * Created by kevincepria on 11/15/2015.
+ */
+public interface MyInterface {
+
+
+    public void pickDateDisplayDay();
+
+
+    public void setText(String message);
+
+
+    public void fadeAnimation(TextView tv);
+}
